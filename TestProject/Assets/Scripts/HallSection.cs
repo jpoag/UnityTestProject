cc0fcad692c2b4c018643f946f7446f95c78c69b
James Poag <James.Poag@gmail.com>
2016-12-17 14:27:51 -0500

    Setup First Hall Prefab

---
+++ TestProject/Assets/Scripts/HallSection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class HallSection : MonoBehaviour
+    {
+        public Magnet[] magnets;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
