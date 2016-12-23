7e0a97bf79ac5591ba9847a07731a41c51edf713
James Poag <James.Poag@gmail.com>
2016-12-23 11:38:28 -0500

    *start Dev Input

---
+++ TestProject/Assets/Scripts/DevInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class DevInput : MonoBehaviour
+    {
+        public MazeBuilder mMazeBuilder;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+
+            }
+        }
+    }
+}
